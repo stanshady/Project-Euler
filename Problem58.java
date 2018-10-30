80fde8875c988d7c32c1d6253544e2402e90b33e
stanshady <44624103+stanshady@users.noreply.github.com>
2018-10-30 17:40:02 -0600

    Update Problem58.java

--- Problem58.java
+++ Problem58.java
@@ -1,8 +1,8 @@
-
 public class Problem58
 {
     public static int[] primes = Algorithm.sieve(2200000);
-    
+    public static final int MAX_PRIME = primes[primes.length - 1];
+
     public static int findLessThan10()
     {
         int diagNum1 = 1;
@@ -12,7 +12,7 @@ public class Problem58
         int numPrimes = 0;
         int totalDiag = 1;
         int sideLen = 1;
-        
+
         int iter = 0;
         while(true)
         {
@@ -21,34 +21,62 @@ public class Problem58
             
             gap1 += 2;
             diagNum1 += gap1;
-            //System.out.println(diagNum1);
-            if(Algorithm.binSearch(primes,diagNum1) > -1)
-                numPrimes++;
-            gap1 += 2;
-            diagNum1 += gap1;
-            //System.out.println(diagNum1);
-            if(Algorithm.binSearch(primes,diagNum1) > -1)
+            if(diagNum1 <= MAX_PRIME)
+            {
+                if(Algorithm.binSearch(primes,diagNum1) > -1)
+                    numPrimes++;
+            }
+            else if(isPrime(diagNum1))
                 numPrimes++;
             
+            gap1 += 2;
+            diagNum1 += gap1; 
+            if(diagNum1 <= MAX_PRIME)
+            {
+                if(Algorithm.binSearch(primes,diagNum1) > -1)
+                    numPrimes++;
+            }
+            else if(isPrime(diagNum1))
+                numPrimes++;
+                
             gap2 += 4;
             diagNum2 += gap2;
-            //System.out.println(diagNum2);
-            if(Algorithm.binSearch(primes,diagNum2) > -1)
+            if(diagNum2 <= MAX_PRIME)
+            {
+                if(Algorithm.binSearch(primes,diagNum2) > -1)
+                    numPrimes++;
+            }
+            else if(isPrime(diagNum2))
                 numPrimes++;
+                
             diagNum2 += gap2;
-            //System.out.println(diagNum2);
-            if(Algorithm.binSearch(primes,diagNum2) > -1)
+            if(diagNum2 <= MAX_PRIME)
+            {
+                if(Algorithm.binSearch(primes,diagNum2) > -1)
+                    numPrimes++;
+            }
+            else if(isPrime(diagNum2))
                 numPrimes++;
-            
+
             if((double) numPrimes / totalDiag < 0.1)       
             {
                 System.out.println(diagNum2);
                 return sideLen;
             }
-                
+
             iter ++;
         }
-        
+
     }
-    
+
+    public static boolean isPrime(long n)
+    {
+        for(long i = 2; i <= Math.sqrt(n); i++)
+        {
+            if(n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
 }
